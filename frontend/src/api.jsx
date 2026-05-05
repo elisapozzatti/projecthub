41213e7394c2ba5b20a1b1cd48d0aee9d22eb73b
elisapozzatti <elisa.pozzatti@gmail.com>
2026-05-05 09:18:27 +0200

    .

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://projecthub-9l9g.onrender.com",
 });
 
 // Funzione per aggiornare il token dopo il login
